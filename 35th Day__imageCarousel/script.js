2cdacf3b3430930581e94f276ac9fd4c7261c94d
NataliaSova <rot.fuchslein@gmail.com>
2021-08-08 15:17:38 +0500

    Image Carousel project

--- 35th Day__imageCarousel/script.js
+++ 35th Day__imageCarousel/script.js
@@ -1,1 +1,43 @@
+const carousel = document.querySelector('.carousel');
 
+if (carousel) {
+  const imgContainer = document.getElementById('imgs');
+  const images = document.querySelectorAll('#imgs img');
+  const prevBtn = document.getElementById('left');
+  const nextBtn = document.getElementById('right');
+
+  let index = 0;
+  let interval = setInterval(run, 2000);
+
+  function run() {
+    index++;
+    changeImage();
+  }
+
+  function changeImage() {
+    if (index > images.length - 1) {
+      index = 0;
+    } else if (index < 0) {
+      index = images.length - 1;
+    }
+
+    imgContainer.style.transform = `translateX(${-index * 500}px)`;
+  }
+
+  function resetInterval() {
+    clearInterval(interval);
+    interval = setInterval(run, 2000);
+  }
+
+  prevBtn.addEventListener('click', () => {
+    index--;
+    changeImage();
+    resetInterval();
+  });
+
+  nextBtn.addEventListener('click', () => {
+    index++;
+    changeImage();
+    resetInterval();
+  });
+}
