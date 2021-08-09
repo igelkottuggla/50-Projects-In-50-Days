02a0be9ed09787b853ac7dcacee97028960f78b5
NataliaSova <rot.fuchslein@gmail.com>
2021-08-09 10:46:20 +0500

    Hoverboard project

--- 36th Day__hoverboard/script.js
+++ 36th Day__hoverboard/script.js
@@ -1,1 +1,47 @@
+const squaresContainer = document.getElementById('squares-container');
+const colors = [
+  'aqua',
+  'black',
+  'blue',
+  'fuchsia',
+  'gray',
+  'green',
+  'lime',
+  'maroon',
+  'navy',
+  'olive',
+  'orange',
+  'purple',
+  'red',
+  'silver',
+  'teal',
+  'white',
+  'yellow',
+];
 
+const SQUARES = 500;
+
+for (let squaresAmount = 0; squaresAmount < SQUARES; squaresAmount++) {
+  const square = document.createElement('div');
+  square.classList.add('square');
+
+  square.addEventListener('mouseover', () => setColor(square));
+  square.addEventListener('mouseout', () => removeColor(square));
+
+  squaresContainer.appendChild(square);
+}
+
+function setColor(element) {
+  const color = getRandomColor();
+  element.style.backgroundColor = color;
+  element.style.boxShadow = `0 0 2px ${color}, 0 0 10px ${color}`;
+}
+
+function removeColor(element) {
+  element.style.backgroundColor = '#1d1d1d';
+  element.style.boxShadow = `0 0 2px #000000`;
+}
+
+function getRandomColor() {
+  return colors[Math.floor(Math.random() * colors.length)];
+}
