53a7d5c72ed5cda705d04a892edf4c3b42662cf3
NataliaSova <rot.fuchslein@gmail.com>
2021-08-10 17:16:12 +0500

    Password Strength Background project

--- 39th Day__passwordStrengthBackground/script.js
+++ 39th Day__passwordStrengthBackground/script.js
@@ -1,1 +1,14 @@
+'use strict';
 
+const password = document.getElementById('password');
+const background = document.querySelector('.background');
+const INITIAL_BLUR = 20;
+const STEP = 2;
+
+password.addEventListener('input', (event) => {
+    const input = event.target.value;
+    const length = input.length;
+
+    const blurValue = INITIAL_BLUR - length * STEP;
+    background.style.filter = `blur(${blurValue}px)`;
+});
