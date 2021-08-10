64c8ccc576536ab3c709fd511ff237b68453639e
NataliaSova <rot.fuchslein@gmail.com>
2021-08-10 12:56:10 +0500

    Mobile Tab navigation + replaced using position absolute with flexbox

--- 38th Day__mobileTabNavigation/script.js
+++ 38th Day__mobileTabNavigation/script.js
@@ -1,1 +1,20 @@
+const content = document.querySelectorAll('.content');
+const listItems = document.querySelectorAll('.item');
 
+listItems.forEach((item, index) => {
+    item.addEventListener('click', () => {
+        hideAllContents();
+        hideAllItems();
+
+        item.classList.add('active');
+        content[index].classList.add('show');
+    });
+});
+
+function hideAllContents() {
+    content.forEach((content) => content.classList.remove('show'));
+}
+
+function hideAllItems() {
+    listItems.forEach((item) => item.classList.remove('active'));
+}
