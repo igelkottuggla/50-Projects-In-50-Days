026c1e2e0062597eeb3ad174092d92118278586d
NataliaSova <rot.fuchslein@gmail.com>
2021-08-11 15:19:19 +0500

    Verify account UI + developed futher media queries

--- 41st Day__verifyAccountUI/script.js
+++ 41st Day__verifyAccountUI/script.js
@@ -1,1 +1,17 @@
+'use strict';
 
+const codeInputs = document.querySelectorAll('.code');
+const DELETE_KEY = 'Delete';
+const BCSPACE_KEY = 'Backspace';
+codeInputs[0].focus();
+
+codeInputs.forEach((input, index) => {
+    input.addEventListener('keydown', (event) => {
+        if (event.key >= 0 && event.key <= 9) {
+            codeInputs[index].value = '';
+            setTimeout(() => codeInputs[index + 1].focus(), 10);
+        } else if (event.key === BCSPACE_KEY || event.key === DELETE_KEY) {
+            setTimeout(() => codeInputs[index - 1].focus(), 10);
+        }
+    });
+});
