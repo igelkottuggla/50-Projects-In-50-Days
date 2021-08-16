f6799aaa7cf31c7433634dd3f33548022b45ee29
NataliaSova <rot.fuchslein@gmail.com>
2021-08-16 10:19:19 +0500

    Random Image feed + added input for the user to choose the amount of rows and btn to get the pictures based on the choosen amout

--- 48th Day__randomImageGenerator/script.js
+++ 48th Day__randomImageGenerator/script.js
@@ -1,1 +1,24 @@
+const imgContainer = document.querySelector('.image-container');
+const rowsValue = document.querySelector('.amount-input');
+const getImgsBtn = document.querySelector('.btn');
 
+const unsplashURL = 'https://source.unsplash.com/random/';
+const imagesPerRow = 3;
+
+getImgsBtn.addEventListener('click', () => {
+    imgContainer.innerHTML = '';
+    const rows = rowsValue.value;
+    for (let i = 0; i < rows * imagesPerRow; i++) {
+        const img = document.createElement('img');
+        img.src = `${unsplashURL}${getRandomSize()}`;
+        imgContainer.appendChild(img);
+    }
+});
+
+function getRandomSize() {
+    return `${getRandomNumber()}x${getRandomNumber()}`;
+}
+
+function getRandomNumber() {
+    return Math.floor(Math.random() * 10) + 300;
+}
